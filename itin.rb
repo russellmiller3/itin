6d71d76efbf88511bf4391338f25f84045f00799
Russell Miller <russell@adamm.net>
2014-11-12 16:19:19 -0800

    Added Layout and Home templates

--- itin.rb
+++ itin.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'sinatra/partial'
+require 'better_errors'
 
-get "/" do |variable|
-	"Itin - The shareable, editable travel itinerary"
+require_relative 'config/dotenv'
+require_relative 'models'
+
+set :partial_template_engine, :erb
+
+configure :development do
+	use BetterErrors::Middleware
+	BetterErrors.application_root  = File.expand_path('..', __FILE__)
+
+get "/" do
+
+	erb :'itin/home'
 end
