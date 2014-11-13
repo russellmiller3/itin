7d275fcbf5a7a46f851625f89883f35e60d62b2e
Russell Miller <russell@adamm.net>
2014-11-12 20:17:27 -0800

    Created the Itin & Event Models

--- Gemfile
+++ Gemfile
@@ -1,8 +1,17 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-partial'
+gem 'data_mapper'
 
 group :development do
+		gem 'sqlite3'
 		gem 'rerun'
+		gem 'dm-sqlite-adapter'
+		gem 'dotenv'
+end
 
+group :production do
+		gem 'dm-postgres-adapter'
+		gem 'pg'
 end
---
+++ models.rb
@@ -0,0 +1,39 @@
+require 'data_mapper'
+
+if ENV['RACK_ENV'] != "production"
+	require 'dotenv'
+	Dotenv.load('.env')
+	DataMapper::Logger.new(STDOUT, :debug)
+	DataMapper.setup(:default, "sqlite:itin.db")
+end
+
+if ENV['RACK_ENV'] == "production"
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+class Itin
+	include DataMapper::Resource
+
+property :id, Serial
+property :title, Text
+property :notes, Text
+property :created_at, DateTime
+has n, :event
+
+end
+
+class Event
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :description, Text
+	property :start_time, Time
+	property :start_date, Date
+	property :end_time, Time
+	property :end_date, Date
+	belongs_to :itin
+
+end
+
+DataMapper.finalize()
+DataMapper.auto_upgrade!()
